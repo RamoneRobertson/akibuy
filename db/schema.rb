0f6f06aa5cab5358a61cbc0138f4968b4ac2ac35
Ramone Robertson <ramonerobertson10@gmail.com>
2025-01-02 21:59:56 +0900

    re-added transaction model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_02_125829) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_02_125928) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -50,6 +50,18 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_02_125829) do
     t.index ["user_id"], name: "index_bids_on_user_id"
   end
 
+  create_table "transactions", force: :cascade do |t|
+    t.integer "payment_mthod"
+    t.integer "amount"
+    t.integer "payment_status"
+    t.bigint "auction_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["auction_id"], name: "index_transactions_on_auction_id"
+    t.index ["user_id"], name: "index_transactions_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -66,4 +78,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_02_125829) do
   add_foreign_key "auctions", "users"
   add_foreign_key "bids", "auctions"
   add_foreign_key "bids", "users"
+  add_foreign_key "transactions", "auctions"
+  add_foreign_key "transactions", "users"
 end
