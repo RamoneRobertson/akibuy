2ad11302310966c952987418255685df81118954
Ramone Robertson <ramonerobertson10@gmail.com>
2025-01-08 13:55:53 +0900

    Finished update method

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -19,9 +19,18 @@ class Api::V1::ProductsController < ApplicationController
     end
   end
 
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      render json: { product: @product, message: "Product updated successfully" }, status: :ok
+    else
+      render json: { error: product.errors.messages }, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def product_params
-    params.require(:product).permit(:name, :image, :categories, :condition, :description, :shipping_method, :region_of_origin, :sales_price, :delivery_time)
+    params.require(:product).permit(:name, :image, :categories, :condition, :description, :shipping_method, :region_of_origin, :sales_price, :delivery_time, :user_id)
   end
 end
