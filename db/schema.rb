856e1ad66dcbe16b02da9ade54205dcdb21bc0ca
Ramone Robertson <ramonerobertson10@gmail.com>
2025-01-10 18:20:53 +0900

    Added cart model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_06_080148) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_10_092026) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "carts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_carts_on_user_id"
+  end
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
@@ -61,6 +68,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_06_080148) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "carts", "users"
   add_foreign_key "comments", "products", column: "products_id"
   add_foreign_key "comments", "users", column: "users_id"
   add_foreign_key "products", "users"
